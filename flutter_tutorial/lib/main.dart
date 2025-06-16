25fcc933c8e501c578f6f2b9f988170f30dec9ad
jeevandas-jd <jeevandasms2004@gmail.com>
2025-06-16 18:06:28 +0530

    flutterRowAndColumnWidgets

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -1,34 +1,49 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
+void main() {
+  runApp(MyApp());
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.amber), home: Homescreen());
-  }
-}
-
-class Homescreen extends StatelessWidget {
-  const Homescreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 231, 180, 10),
-      appBar: AppBar(
-        title: Text('My App'),
-      ),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-void main() {
-  runApp(MyWidget());
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Column Layout Example'),
+        backgroundColor: Colors.teal,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 150,
+            width: double.infinity,
+            color: Colors.red,
+            child: Center(
+                child: Text('Container 1',
+                    style: TextStyle(color: Colors.white, fontSize: 20))),
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: 150,
+            width: double.infinity,
+            color: Colors.blue,
+            child: Center(
+                child: Text('Container 2',
+                    style: TextStyle(color: Colors.white, fontSize: 20))),
+          ),
+        ],
+      ),
+    );
+  }
 }
